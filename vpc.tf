cb3e0ce1e40e6376ae8e0454dd9affe1a56b7f34
Abhishek Srivastava <cs.abhilive@gmail.com>
2021-05-21 13:39:18 +0000

    changes in VPC configuration

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 module "network" {
   source  = "app.terraform.io/abhishek-srivastava-training/network/google"
   version = "2.5.0"
-  network_name = "gaurav-network"
+  network_name = "abhilive-network"
   project_id = var.project
   subnets = [
     {
-        subnet_name   = "gaurav-subnet"
+        subnet_name   = "abhilive-subnet"
         subnet_ip     = "10.100.10.0/24"
         subnet_region = var.region
     }
